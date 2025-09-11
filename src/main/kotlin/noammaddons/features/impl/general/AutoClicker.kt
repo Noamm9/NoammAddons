85751e393cadd53308eaead805ca666b5160267f
Noamm <belkunsama@gmail.com>
2025-09-12 01:29:39 +0300

    make AutoClicker not ban

--- src/main/kotlin/noammaddons/features/impl/general/AutoClicker.kt
+++ src/main/kotlin/noammaddons/features/impl/general/AutoClicker.kt
@@ -1,63 +1,71 @@
 package noammaddons.features.impl.general
 
+import net.minecraft.client.settings.KeyBinding
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent.*
 import noammaddons.features.Feature
-import noammaddons.ui.config.core.impl.KeybindSetting
-import noammaddons.ui.config.core.impl.SliderSetting
-import noammaddons.ui.config.core.impl.ToggleSetting
+import noammaddons.ui.config.core.impl.*
 import noammaddons.utils.ItemUtils.skyblockID
-import noammaddons.utils.PlayerUtils.leftClick
-import noammaddons.utils.PlayerUtils.rightClick
 import noammaddons.utils.ServerPlayer
 import org.lwjgl.input.Keyboard
 
+
 object AutoClicker: Feature(name = "Auto Clicker") {
-    private val leftClickToggle = ToggleSetting("Left Click")
-    private val leftClickKeybind = KeybindSetting("Left Click Keybind").addDependency(leftClickToggle)
-    private val rightClickToggle = ToggleSetting("Right Click")
-    private val rightClickKeybind = KeybindSetting("Right Click Keybind").addDependency(rightClickToggle)
+    private val cps = SliderSetting("Clicks Per Second", 3f, 15f, .5f, 5f)
     private val terminatorCheck = ToggleSetting("Terminator Only")
-    private val cps by SliderSetting("Clicks Per Second", 3f, 15f, .5f, 5f)
-    private var nextLeftClick = .0
-    private var nextRightClick = .0
+    private val leftClickToggle = ToggleSetting("Left Click").addDependency { terminatorCheck.value }
+    private val leftClickKeybind = KeybindSetting("Left Click Keybind").addDependency(leftClickToggle).addDependency { terminatorCheck.value }
+    private val rightClickToggle = ToggleSetting("Right Click").addDependency { terminatorCheck.value }
+    private val rightClickKeybind = KeybindSetting("Right Click Keybind").addDependency(rightClickToggle).addDependency { terminatorCheck.value }
+
+    private var nextLeftClick = 0L
+    private var nextRightClick = 0L
 
     override fun init() {
         addSettings(
+            cps, terminatorCheck,
             leftClickToggle, leftClickKeybind,
-            rightClickToggle, rightClickKeybind, terminatorCheck
+            rightClickToggle, rightClickKeybind,
         )
     }
 
     @SubscribeEvent
     fun onClientTickEvent(event: ClientTickEvent) {
         if (event.phase != Phase.START) return
-        if (mc.currentScreen != null) return
+        if (mc.currentScreen != null || mc.thePlayer == null) return
+        if (mc.thePlayer.isUsingItem) return
         val now = System.currentTimeMillis()
 
         if (terminatorCheck.value) {
-            if (!mc.gameSettings.keyBindUseItem.isKeyDown) return
+            if (! mc.gameSettings.keyBindUseItem.isKeyDown) return
             if (ServerPlayer.player.getHeldItem().skyblockID != "TERMINATOR") return
             if (now < nextLeftClick) return
 
-            nextLeftClick = now + ((1000 / cps) + ((Math.random() - .5) * 60.0))
-            leftClick()
-        } else {
+            nextLeftClick = getNextClick(now)
+            KeyBinding.onTick(mc.gameSettings.keyBindAttack.keyCode)
+        }
+        else {
             if (leftClickToggle.value) {
-                if (!Keyboard.isKeyDown(leftClickKeybind.value)) return
+                if (! Keyboard.isKeyDown(leftClickKeybind.value)) return
                 if (now < nextLeftClick) return
 
-                nextLeftClick = now + ((1000 / cps) + ((Math.random() - .5) * 60.0))
-                leftClick()
+                nextLeftClick = getNextClick(now)
+                KeyBinding.onTick(mc.gameSettings.keyBindAttack.keyCode)
             }
 
             if (rightClickToggle.value) {
-                if (!Keyboard.isKeyDown(rightClickKeybind.value)) return
+                if (! Keyboard.isKeyDown(rightClickKeybind.value)) return
                 if (now < nextRightClick) return
 
-                nextRightClick = now + ((1000 / cps) + ((Math.random() - .5) * 60.0))
-                rightClick()
+                nextRightClick = getNextClick(now)
+                KeyBinding.onTick(mc.gameSettings.keyBindUseItem.keyCode)
             }
         }
+    }
+
+    private fun getNextClick(now: Long): Long {
+        val delay = (1000.0 / cps.value).toLong()
+        val randomOffset = (Math.random() * 60.0 - 30.0).toLong()
+        return now + delay + randomOffset
     }
 }
