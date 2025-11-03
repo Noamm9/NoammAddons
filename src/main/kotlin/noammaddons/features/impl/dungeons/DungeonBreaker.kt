e0886aac2a0b85ce2b4ecb8c5eae5f05ecec766c
Noamm <belkunsama@gmail.com>
2025-11-03 16:20:09 +0200

    added Puzzle check for Zero Ping DungeonBreaker

--- src/main/kotlin/noammaddons/features/impl/dungeons/DungeonBreaker.kt
+++ src/main/kotlin/noammaddons/features/impl/dungeons/DungeonBreaker.kt
@@ -3,11 +3,11 @@ package noammaddons.features.impl.dungeons
 import net.minecraft.init.Blocks
 import net.minecraft.util.BlockPos
 import noammaddons.features.Feature
+import noammaddons.features.impl.dungeons.dmap.core.map.RoomType
 import noammaddons.ui.config.core.impl.ToggleSetting
+import noammaddons.utils.*
 import noammaddons.utils.BlockUtils.getBlockAt
 import noammaddons.utils.ItemUtils.skyblockID
-import noammaddons.utils.LocationUtils
-import noammaddons.utils.ServerPlayer
 
 object DungeonBreaker: Feature() {
     private val zeropingbreaker by ToggleSetting("Zero Ping")
@@ -18,12 +18,16 @@ object DungeonBreaker: Feature() {
         Blocks.piston_head, Blocks.piston, Blocks.piston_extension, Blocks.lever, Blocks.stone_button
     )
 
+    /**
+     * @see noammaddons.mixins.MixinPlayerControllerMP
+     */
     @JvmStatic
     fun onHitBlock(pos: BlockPos) {
         if (! enabled) return
         if (! zeropingbreaker) return
         if (! LocationUtils.inDungeon) return
         if (ServerPlayer.player.getHeldItem().skyblockID != "DUNGEONBREAKER") return
+        if (ScanUtils.currentRoom?.data?.type == RoomType.PUZZLE) return
         val block = getBlockAt(pos).takeUnless { it in blacklist } ?: return
 
         mc.theWorld.setBlockToAir(pos)
