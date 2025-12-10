d289715eb06393746ed7c5a92d929aa672c2a670
Noamm <belkunsama@gmail.com>
2025-12-11 01:07:43 +0200

    LifelineHud: better lifeline hp logic

--- src/main/kotlin/noammaddons/features/impl/hud/LifelineHud.kt
+++ src/main/kotlin/noammaddons/features/impl/hud/LifelineHud.kt
@@ -1,9 +1,7 @@
 package noammaddons.features.impl.hud
 
-import net.minecraft.network.play.server.S06PacketUpdateHealth
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import noammaddons.config.EditGui.GuiElement
-import noammaddons.events.MainThreadPacketRecivedEvent
 import noammaddons.events.RenderOverlay
 import noammaddons.features.Feature
 import noammaddons.utils.RenderHelper
@@ -18,13 +16,7 @@ object LifelineHud: Feature("Shows on screen when u are below 20%") {
         override fun draw() = drawText(text, getX(), getY(), getScale())
     }
 
-    private var lifelineActive = false
-
-    @SubscribeEvent
-    fun onPacket(event: MainThreadPacketRecivedEvent.Post) {
-        val packet = event.packet as? S06PacketUpdateHealth ?: return
-        lifelineActive = packet.health < (mc.thePlayer.maxHealth * 0.195)
-    }
+    private val lifelineActive get() = mc.thePlayer.run { (health + absorptionAmount) < (maxHealth * 0.195) }
 
     @SubscribeEvent
     fun draw(event: RenderOverlay) {
