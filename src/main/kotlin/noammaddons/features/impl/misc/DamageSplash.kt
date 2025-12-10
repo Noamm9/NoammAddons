dcc034a50f25a76fc2d92826e9736b49dd5f0432
Noamm <belkunsama@gmail.com>
2025-12-11 00:08:44 +0200

    IMPROVE: DamageSplash
    added a toggle change the format to be in uppercase.
    Added an option to disable damage numbers in dungeon clear to improve fps (Strikexs this is for you 🙏 )
    
    refactor the event listener to use MainThreadPacketRecivedEvent.Pre to reduce the blocking on the packet thread

--- src/main/kotlin/noammaddons/features/impl/misc/DamageSplash.kt
+++ src/main/kotlin/noammaddons/features/impl/misc/DamageSplash.kt
@@ -1,27 +1,29 @@
 package noammaddons.features.impl.misc
 
-
 import net.minecraft.network.play.server.S0FPacketSpawnMob
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import noammaddons.events.PacketEvent
+import noammaddons.events.MainThreadPacketRecivedEvent
 import noammaddons.features.Feature
 import noammaddons.ui.config.core.impl.ToggleSetting
 import noammaddons.utils.ChatUtils.addColor
 import noammaddons.utils.ChatUtils.removeFormatting
 import noammaddons.utils.LocationUtils.inBoss
+import noammaddons.utils.LocationUtils.inDungeon
 import noammaddons.utils.LocationUtils.inSkyblock
 import noammaddons.utils.NumbersUtils.format
 import noammaddons.utils.Utils.containsOneOf
 
 object DamageSplash: Feature() {
     //  private val mode = DropdownSetting("Mode", arrayListOf("Minimal", "Decimal comma"))
-    private val disableinBoss by ToggleSetting("Disable in Boss")
+    private val uppercase by ToggleSetting("Uppercase Damage Formatting")
+    private val disableinBoss by ToggleSetting("Disable in Dungeon Boss Room")
+    private val disableinClear by ToggleSetting("Disable in Dungeon Clear")
 
     // Thanks Skytils 😘
     val damageRegex = Regex("[✧✯]?(\\d{1,3}(?:,\\d{3})*[⚔+✧❤♞☄✷ﬗ✯]*)")
 
     @SubscribeEvent
-    fun handleCustomDamageSplash(event: PacketEvent.Received) {
+    fun handleCustomDamageSplash(event: MainThreadPacketRecivedEvent.Pre) {
         if (! inSkyblock) return
         val packet = event.packet as? S0FPacketSpawnMob ?: return
         if (packet.entityType != 30) return // Armor stand
@@ -31,9 +33,11 @@ object DamageSplash: Feature() {
         val name = "${nameData.getObject()}".removeFormatting()
         val damage = damageRegex.matchEntire(name)?.destructured?.component1() ?: return
         if (inBoss && disableinBoss) return event.setCanceled(true)
+        if (inDungeon && ! inBoss && disableinClear) return event.setCanceled(true)
+        val formatted = if (uppercase) format(damage).uppercase() else format(damage)
 
-        val newName = if (name.containsOneOf("✧", "✯")) "&f✧${addRandomColorCodes(format(damage))}&f✧"
-        else "&3${format(damage)}"
+        val newName = if (name.containsOneOf("✧", "✯")) "&f✧${addRandomColorCodes(formatted)}&f✧"
+        else "&3${formatted}"
 
         nameData.setObject(newName.addColor())
     }
