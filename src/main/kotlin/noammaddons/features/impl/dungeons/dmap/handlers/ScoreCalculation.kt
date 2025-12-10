83cd7a3929d8f285c7e544b44ebe170ecb184a96
Noamm <belkunsama@gmail.com>
2025-12-11 00:14:54 +0200

    Update puzzle penalty logic in score calculation
    Puzzle penalty now only count for CLEARED state if the puzzle is Blaze

--- src/main/kotlin/noammaddons/features/impl/dungeons/dmap/handlers/ScoreCalculation.kt
+++ src/main/kotlin/noammaddons/features/impl/dungeons/dmap/handlers/ScoreCalculation.kt
@@ -7,6 +7,7 @@ import net.minecraft.network.play.server.S3EPacketTeams
 import noammaddons.features.impl.alerts.CryptsDone
 import noammaddons.features.impl.dungeons.MimicDetector
 import noammaddons.features.impl.dungeons.ScoreCalculator
+import noammaddons.features.impl.dungeons.dmap.core.map.Puzzle
 import noammaddons.features.impl.dungeons.dmap.core.map.RoomState
 import noammaddons.utils.DungeonUtils
 import noammaddons.utils.DungeonUtils.puzzles
@@ -62,7 +63,9 @@ object ScoreCalculation {
             val completedRoomScore = (effectiveCompletedRooms.toDouble() / totalRooms.toDouble() * 60.0).coerceIn(.0, 60.0).toInt()
 
             val skillRooms = floor(effectiveCompletedRooms.toDouble() / totalRooms.toDouble() * 80f).coerceIn(.0, 80.0).toInt()
-            val puzzlePenalty = (puzzles.size - puzzles.count { it.state.equalsOneOf(RoomState.GREEN, RoomState.CLEARED) }) * 10
+            val puzzlePenalty = (puzzles.size - puzzles.count {
+                (it.state == RoomState.CLEARED && it.equalsOneOf(Puzzle.HIGHER_BLAZE, Puzzle.LOWER_BLAZE)) || it.state == RoomState.GREEN
+            }) * 10
             val deathPenalty = (deathCount * 2 - 1).coerceAtLeast(0)
 
             val score = secretsScore + completedRoomScore + (20 + skillRooms - puzzlePenalty - deathPenalty).coerceIn(20, 100) + bonusScore + speedScore
