117fe57ef756b40f353c0502c27f8316aa730ad1
Noamm <belkunsama@gmail.com>
2025-12-21 01:22:29 +0200

    set increment to 10

--- src/main/kotlin/noammaddons/features/impl/hud/FreezeDisplay.kt
+++ src/main/kotlin/noammaddons/features/impl/hud/FreezeDisplay.kt
@@ -6,9 +6,7 @@ import noammaddons.config.editgui.HudEditorScreen
 import noammaddons.events.RenderOverlay
 import noammaddons.events.ServerTick
 import noammaddons.features.Feature
-import noammaddons.ui.config.core.impl.ColorSetting
-import noammaddons.ui.config.core.impl.SliderSetting
-import noammaddons.ui.config.core.impl.ToggleSetting
+import noammaddons.ui.config.core.impl.*
 import noammaddons.utils.LocationUtils
 import noammaddons.utils.RenderHelper.getStringWidth
 import noammaddons.utils.RenderUtils.drawText
@@ -16,7 +14,7 @@ import java.awt.Color
 
 object FreezeDisplay: Feature(desc = "Shows how long the server froze after a chosen threshold.") {
     private val color by ColorSetting("Color", Color(245, 73, 39), false)
-    private val threshold by SliderSetting("Threshold", 50, 2000, 1, 500)
+    private val threshold by SliderSetting("Threshold", 50, 2000, 10, 500)
     private val dungeonsOnly by ToggleSetting("Only In Dungeons", true)
 
     private var lastTick = System.currentTimeMillis()
@@ -28,7 +26,7 @@ object FreezeDisplay: Feature(desc = "Shows how long the server froze after a ch
         override val height: Float get() = 9f
         override fun draw() {
             if (HudEditorScreen.isOpen()) return
-            if (dungeonsOnly && !LocationUtils.inDungeon) return
+            if (dungeonsOnly && ! LocationUtils.inDungeon) return
             val tickDelta = System.currentTimeMillis() - lastTick
             if (tickDelta < threshold) return
 
@@ -40,7 +38,7 @@ object FreezeDisplay: Feature(desc = "Shows how long the server froze after a ch
 
     @SubscribeEvent
     fun draw(event: RenderOverlay) {
-        if (!FreezeDisplayElement.enabled) return
+        if (! FreezeDisplayElement.enabled) return
         FreezeDisplayElement.draw()
     }
 
