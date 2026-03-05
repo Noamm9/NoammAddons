e1f33540d528b17ac9ba0db605c128598da160f3
Noamm <belkunsama@gmail.com>
2026-03-05 23:15:29 +0200

    fix typo

--- src/main/kotlin/com/github/noamm9/features/impl/dungeon/M7Relics.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/M7Relics.kt
@@ -33,7 +33,7 @@ object M7Relics: Feature(name = "M7 Relics", description = "A bunch of M7 Relics
     private val relicBox by ToggleSetting("Box Relics").withDescription("Draws a box on where the relics are spawning and the cauldron you need to place.")
     private val relicSpawnTimer by ToggleSetting("Spawn Timer").withDescription("Shows on screen when the relic will spawn.")
     private val relicTimer by ToggleSetting("Place Timer").withDescription("Sends in chat the time it took to place the relic after you picked it up.")
-    private val relicLook by ToggleSetting("Relic Look").withDescription("Automatically rotate to the relic cauldron after yoo pick it up.")
+    private val relicLook by ToggleSetting("Relic Look").withDescription("Automatically rotate to the relic cauldron after you pick it up.")
     private val relicLookTime by SliderSetting("Relic Look Time", 150L, 10, 300, 1).showIf { relicLook.value }.withDescription("How fast should the auto rotate (in milliseconds)")
     private val blockWrongRelic by ToggleSetting("Block Wrong Relic").withDescription("Prevents you from placing your relic at the wrong cauldron.")
 
