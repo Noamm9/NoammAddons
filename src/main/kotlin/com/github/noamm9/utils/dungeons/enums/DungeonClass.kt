4200ecb2b30b330365c8df9a7beebbee60363b95
mvp-u <mvpu3900@gmail.com>
2026-03-15 20:09:37 -0400

    Update DungeonClass.kt

--- src/main/kotlin/com/github/noamm9/utils/dungeons/enums/DungeonClass.kt
+++ src/main/kotlin/com/github/noamm9/utils/dungeons/enums/DungeonClass.kt
@@ -3,8 +3,8 @@ package com.github.noamm9.utils.dungeons.enums
 import java.awt.Color
 
 enum class DungeonClass(val color: Color, val code: String) {
-    Archer(Color(125, 0, 0), "§4"),
-    Berserk(Color(205, 106, 0), "§6"),
+    Archer(Color(205, 106, 0), "§6"), // switched archer and bers to be correct.
+    Berserk(Color(125, 0, 0), "§4"),
     Healer(Color(123, 0, 123), "§5"),
     Mage(Color(0, 185, 185), "§3"),
     Tank(Color(0, 125, 0), "§2"),
