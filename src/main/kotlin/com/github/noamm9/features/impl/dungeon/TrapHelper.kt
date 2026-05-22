d34794f648b50d65e191e33f9f753b25ae0e0b3b
dsgameover <dsgameover@users.noreply.github.com>
2026-05-23 02:48:43 +0300

    made it use event.room.rotation

--- src/main/kotlin/com/github/noamm9/features/impl/dungeon/TrapHelper.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/TrapHelper.kt
@@ -7,38 +7,31 @@ import com.github.noamm9.features.Feature
 import com.github.noamm9.ui.clickgui.components.getValue
 import com.github.noamm9.ui.clickgui.components.provideDelegate
 import com.github.noamm9.ui.clickgui.components.impl.ColorSetting
-import com.github.noamm9.utils.dungeons.map.core.UniqueRoom
 import com.github.noamm9.utils.dungeons.map.utils.ScanUtils
 import com.github.noamm9.utils.render.Render3D
 import net.minecraft.core.BlockPos
 import java.awt.Color
-import kotlin.math.abs
 
 object TrapHelper: Feature("Highlights the correct pearl block in New Trap") {
 
     private val color by ColorSetting("Epearl highlight color", Color.MAGENTA)
-
-    private val northEastRelPos = BlockPos(27, 89, 15)
-    private val southWestRelPos = BlockPos(26, 89, 15)
     private var highlightPos: BlockPos? = null
 
     override fun init() {
         register<DungeonEvent.RoomEvent.onEnter> {
             if (event.room.data.name != "New Trap") {
-                highlightPos = null
+                highlightPos = null;
                 return@register
             }
-            val prev = ScanUtils.lastKnownRoom ?: return@register
-            val direction = getDirectionFromRooms(prev, event.room)
             val corner = event.room.corner ?: return@register
-            val rotation = 360 - (event.room.rotation ?: 0)
+            val rotation = event.room.rotation ?: return@register
 
-            val relPos = when (direction) {
-                "North", "East" -> northEastRelPos
-                "South", "West" -> southWestRelPos
+            val relPos = when (rotation) {
+                0, 90 -> BlockPos(26, 89, 15)
+                180, 270 -> BlockPos(27, 89, 15)
                 else -> return@register
             }
-            highlightPos = ScanUtils.getRealCoord(relPos, corner, rotation)
+            highlightPos = ScanUtils.getRealCoord(relPos, corner, 360 - rotation)
         }
 
         register<RenderWorldEvent> {
@@ -46,19 +39,6 @@ object TrapHelper: Feature("Highlights the correct pearl block in New Trap") {
             Render3D.renderBlock(event.ctx, pos, color.value, outline = true, fill = false, phase = true)
         }
 
-        register<WorldChangeEvent> {
-            highlightPos = null
-        }
-    }
-
-    private fun getDirectionFromRooms(from: UniqueRoom, to: UniqueRoom): String {
-        val dx = to.mainRoom.x - from.mainRoom.x
-        val dz = to.mainRoom.z - from.mainRoom.z
-        return if (abs(dx) >= abs(dz)) {
-            if (dx > 0) "East" else "West"
-        }
-        else {
-            if (dz > 0) "South" else "North"
-        }
+        register<WorldChangeEvent> { highlightPos = null }
     }
 }
