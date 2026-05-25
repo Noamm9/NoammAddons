d6e3359fb916fa59255092960c7ea5f3fa5a22e1
Noamm9 <belkunsama@gmail.com>
2026-05-25 14:15:16 +0300

    add ContentEncoding & change from okHttp to CIO

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,9 +57,10 @@ dependencies {
     annotationProcessor("io.github.llamalad7:mixinextras-fabric:0.4.1")
 
     bundled("io.github.classgraph:classgraph:4.8.174")
-    bundled("io.ktor:ktor-client-okhttp-jvm:$ktor_version")
+    bundled("io.ktor:ktor-client-cio:${ktor_version}")
     bundled("io.ktor:ktor-client-websockets-jvm:$ktor_version")
     bundled("io.ktor:ktor-client-content-negotiation-jvm:$ktor_version")
+    bundled("io.ktor:ktor-client-encoding:$ktor_version")
     bundled("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
 
     modImplementation("com.github.Noamm9:DataFixer:7148621a34")
--- src/main/kotlin/com/github/noamm9/utils/network/WebUtils.kt
+++ src/main/kotlin/com/github/noamm9/utils/network/WebUtils.kt
@@ -5,9 +5,10 @@ import com.github.noamm9.NoammAddons.MOD_VERSION
 import com.github.noamm9.utils.JsonUtils
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.UserAgent
+import io.ktor.client.plugins.compression.ContentEncoding
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.request.get
@@ -17,10 +18,23 @@ import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketDeflateExtension
+import java.util.zip.*
 
 object WebUtils {
-    internal val client = HttpClient(OkHttp) {
-        install(WebSockets) { pingIntervalMillis = 30_000 }
+    val client = HttpClient(CIO) {
+        install(WebSockets) {
+            pingIntervalMillis = 30_000
+
+            extensions {
+                install(WebSocketDeflateExtension) {
+                    compressionLevel = Deflater.BEST_SPEED
+                    compressIfBiggerThan(bytes = 1024)
+                    compressIf { frame -> frame is Frame.Text }
+                }
+            }
+        }
         install(UserAgent) { agent = "$MOD_NAME/$MOD_VERSION (+https://noamm.org)" }
         install(HttpTimeout) {
             connectTimeoutMillis = 10_000
@@ -30,6 +44,11 @@ object WebUtils {
         install(ContentNegotiation) {
             json(JsonUtils.json)
         }
+        install(ContentEncoding) {
+            gzip(1.0F)
+            deflate(0.9F)
+        }
+
         expectSuccess = false
     }
 
