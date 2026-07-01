8a7a7d0168a09c1d74eb59f327250cd38de7985e
Noamm9 <belkunsama@gmail.com>
2026-07-02 00:58:10 +0300

    chore(LavaToWater) make config options private

--- src/main/kotlin/com/github/noamm9/features/impl/visual/LavaToWater.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/visual/LavaToWater.kt
@@ -18,8 +18,8 @@ import java.awt.Color
  */
 @Suppress("UNNECESSARY_SAFE_CALL") // mc.levelRenderer is null at game init
 object LavaToWater: Feature("Replaces lava with the water texture") {
-    val colorTint by ToggleSetting("Color Tint", false)
-    val tintColor by ColorSetting("Tint Color", Color(63, 118, 228), false).showIf { colorTint.value }
+    private val colorTint by ToggleSetting("Color Tint", false)
+    private val tintColor by ColorSetting("Tint Color", Color(63, 118, 228), false).showIf { colorTint.value }
     override fun init() = configSettings.forEach { it.onChange { if (enabled) mc.levelRenderer?.allChanged() } }
 
     override fun toggle() {
