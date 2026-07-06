d4cd430560608217d5c3130a0fc42b2813ba16bb
Astrid <homochicken@proton.me>
2026-07-06 21:29:03 +0000

    Inject method to hide Dinnerbone nametag

--- src/main/java/com/github/noamm9/mixin/MixinLivingEntityRenderer.java
+++ src/main/java/com/github/noamm9/mixin/MixinLivingEntityRenderer.java
@@ -7,16 +7,23 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
 public abstract class MixinLivingEntityRenderer {
-    @ModifyExpressionValue(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;D)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isDiscrete()Z"))
-    private boolean forceSneakingNametag(boolean original, LivingEntity entity) {
-        return (!NameTagTweaks.isForceNametagActive() || !(entity instanceof Player)) && original;
-    }
+    @Inject(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;D)Z", at = @At("HEAD"), cancellable = true)
+    private void hideDinnerboneNametag(LivingEntity entity, double distanceToCameraSq, CallbackInfoReturnable<Boolean> cir) {
+        if (NameTagTweaks.shouldHideDinnerbone(entity)) cir.setReturnValue(false);
+    }
 
-    @ModifyExpressionValue(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;D)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isInvisibleTo(Lnet/minecraft/world/entity/player/Player;)Z"))
-    private boolean forceInvisibleNametag(boolean original, LivingEntity entity) {
-        return (!NameTagTweaks.isForceNametagActive() || !(entity instanceof Player) || entity.getUUID().version() != 4) && original;
-    }
+    @ModifyExpressionValue(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;D)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isDiscrete()Z"))
+    private boolean forceSneakingNametag(boolean original, LivingEntity entity) {
+        return (!NameTagTweaks.isForceNametagActive() || !(entity instanceof Player)) && original;
+    }
+
+    @ModifyExpressionValue(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;D)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isInvisibleTo(Lnet/minecraft/world/entity/player/Player;)Z"))
+    private boolean forceInvisibleNametag(boolean original, LivingEntity entity) {
+        return (!NameTagTweaks.isForceNametagActive() || !(entity instanceof Player) || entity.getUUID().version() != 4) && original;
+    }
 }
