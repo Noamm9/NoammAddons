a228e802903c45890f685bc379ccd33b382718c5
Noamm9 <belkunsama@gmail.com>
2026-07-11 16:46:27 +0300

    fix(ProfileUtils) coop stats msgs not being canceled
    fixes https://discord.com/channels/1281979747605418024/1525447398254448861

--- src/main/kotlin/com/github/noamm9/utils/network/ProfileUtils.kt
+++ src/main/kotlin/com/github/noamm9/utils/network/ProfileUtils.kt
@@ -169,15 +169,12 @@ object ProfileUtils {
     private val regex = Regex("^\\w+: \\d+$")
     private var _totalSecrets: Long? = null
     private val chatListener = EventListener.create<ChatMessageEvent> {
-        val text = event.unformattedText
+        if (event.unformattedText == "Secret Counts:") return@create event.cancel()
+        if (! event.unformattedText.matches(regex)) return@create
+        event.isCanceled = true
 
-        if (text == "Secret Counts:") return@create event.cancel()
-        if (text.matches(regex)) {
-            event.cancel()
-            if (text.substringBefore(":") == mc.user.name) {
-                _totalSecrets = text.substringAfter(": ").toLongOrNull()
-                listener.unregister()
-            }
-        }
+        if (event.unformattedText.substringBefore(":") != mc.user.name) return@create
+        _totalSecrets = event.unformattedText.substringAfter(": ").toLongOrNull()
+        ThreadUtils.scheduledTaskServer(5) { listener.unregister() }
     }
 }
