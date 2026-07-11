4bdb86c9ddbaaace96493dae968704139f201ff7
Astrid <homochicken@proton.me>
2026-07-11 17:54:00 +0000

    Revert back to original

--- src/main/kotlin/com/github/noamm9/features/impl/dungeon/solvers/LividSolver.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/solvers/LividSolver.kt
@@ -14,8 +14,6 @@ import com.github.noamm9.utils.Utils.favoriteColor
 import com.github.noamm9.utils.WorldUtils
 import com.github.noamm9.utils.dungeons.DungeonListener.dungeonTeammates
 import com.github.noamm9.utils.location.LocationUtils
-import com.github.noamm9.utils.PlayerUtils
-import com.github.noamm9.utils.items.ItemUtils.skyblockId
 import com.github.noamm9.utils.render.Render2D
 import com.github.noamm9.utils.render.Render2D.width
 import com.github.noamm9.utils.render.Render3D
@@ -34,9 +32,6 @@ object LividSolver: Feature() {
     private val invulnerabilityTimer by ToggleSetting("Invulnerability Timer")
     private val iceSprayTitle by ToggleSetting("Ice Spray Title", true).showIf { invulnerabilityTimer.value }
     private val iceSpraySound by ToggleSetting("Ice Spray Sound", true).showIf { invulnerabilityTimer.value }
-    //#if CHEAT
-    private val autoRagnarock by ToggleSetting("Auto Ragnarock", false).showIf { invulnerabilityTimer.value }
-    //#endif
     private val highlightColor by ColorSetting("Highlight Color", favoriteColor, false).section("Colors")
     private val tracerColor by ColorSetting("Tracer Color", favoriteColor, false).showIf { tracer.value }
 
@@ -52,16 +47,11 @@ object LividSolver: Feature() {
         Blocks.WHITE_WOOL to "Vendetta Livid"
     )
 
-    private var currentLivid: AbstractClientPlayer? = null
     private val ceilingWoolBlock = BlockPos(5, 108, 40)
+    private var lividId: Int? = null
 
     private const val ticks = 390
-    private var timer = -1
-    //#if CHEAT
-    private const val ragnarockSwapTick = 180
-    private const val ragnarockUseTick = 150
-    private var ragnarockUsed = false
-    //#endif
+    private var timer = - 1
 
     override fun init() {
         hudElement("Livid Invulnerability Timer", enabled = { invulnerabilityTimer.value }, shouldDraw = { timer > 0 }, centered = true) { ctx, example ->
@@ -73,26 +63,27 @@ object LividSolver: Feature() {
         }
 
         register<CheckEntityGlowEvent> {
-            if (currentLivid == event.entity) {
+            if (lividId == event.entity.id) {
                 event.color = highlightColor.value
             }
         }
 
         register<CheckEntityRenderEvent> {
-            if (!hideWrong.value) return@register
+            if (! hideWrong.value) return@register
             if (LocationUtils.dungeonFloorNumber != 5) return@register
-            if (!LocationUtils.inBoss) return@register
-            if (currentLivid == event.entity) return@register
-            if (currentLivid?.isSleeping == true) return@register
+            if (! LocationUtils.inBoss) return@register
+            if (lividId == event.entity.id) return@register
+            if (lividId.livid?.isSleeping == true) return@register
             if (dungeonTeammates.any { it.entity?.uuid == event.entity.uuid }) return@register
-            if (event.entity is ArmorStand && !event.entity.name.unformattedText.contains("Livid")) return@register
+            if (event.entity is ArmorStand && ! event.entity.name.unformattedText.contains("Livid")) return@register
             event.isCanceled = true
         }
 
         register<RenderWorldEvent> {
-            val livid = currentLivid ?: return@register
-            if (!livid.isAlive) return@register
+            val livid = lividId.livid ?: return@register
+            if (livid.isDeadOrDying) return@register
             if (livid.isSleeping) return@register
+
             if (tracer.value) Render3D.renderTracer(event.ctx, livid.renderVec.add(y = 0.9), tracerColor.value)
             if (showHp.value) Render3D.renderString(
                 ColorUtils.colorCodeByPercent(livid.health, livid.maxHealth) + NumbersUtils.format(livid.health),
@@ -101,62 +92,36 @@ object LividSolver: Feature() {
         }
 
         register<TickEvent.Start> {
-            if (!LocationUtils.inBoss || LocationUtils.dungeonFloorNumber != 5) return@register
+            if (! LocationUtils.inBoss || LocationUtils.dungeonFloorNumber != 5) return@register
             val targetLivid = lividMap[WorldUtils.getBlockAt(ceilingWoolBlock)] ?: return@register
-            if (currentLivid?.gameProfile?.name == targetLivid && currentLivid?.isRemoved != false) return@register
-            currentLivid = mc.level?.entitiesForRendering()?.asSequence()?.filterIsInstance<AbstractClientPlayer>()?.find {
+            val currentLivid = lividId.livid
+            if (currentLivid?.gameProfile?.name == targetLivid && currentLivid.isRemoved) return@register
+            lividId = mc.level?.entitiesForRendering()?.asSequence()?.filterIsInstance<AbstractClientPlayer>()?.find {
                 it.gameProfile.name == targetLivid
-            }
+            }?.id
         }
 
         register<ChatMessageEvent> {
-            if (!invulnerabilityTimer.value) return@register
+            if (! invulnerabilityTimer.value) return@register
             if (LocationUtils.dungeonFloorNumber != 5) return@register
             if (event.unformattedText != "[BOSS] Livid: Welcome, you've arrived right on time. I am Livid, the Master of Shadows.") return@register
             timer = ticks
-            //#if CHEAT
-            ragnarockUsed = false
-            //#endif
         }
 
         register<TickEvent.Server> {
-            if (timer > 0) timer--
-
-            //#if CHEAT
-            if (autoRagnarock.value) {
-                if (timer == ragnarockSwapTick) {
-                    val slot = PlayerUtils.findHotbarSlot { it.skyblockId.contains("RAGNAROCK") }
-                    if (slot != null) PlayerUtils.swapToSlot(slot)
-                    else ChatUtils.modMessage("&cNo Ragnarock found in hotbar!")
-                }
-
-                if (timer == ragnarockUseTick && !ragnarockUsed) {
-                    val slot = PlayerUtils.findHotbarSlot { it.skyblockId.contains("RAGNAROCK") }
-                    if (slot != null) {
-                        if (mc.player?.inventory?.selectedSlot != slot) PlayerUtils.swapToSlot(slot)
-                        PlayerUtils.rightClick()
-                        ragnarockUsed = true
-                    }
-                }
-            }
-            //#endif
-
+            if (timer > 0) timer --
             if (timer == 0) {
                 if (iceSprayTitle.value) ChatUtils.showTitle("&bIce Spray Livid!")
                 if (iceSpraySound.value) mc.soundManager.play(SimpleSoundInstance.forUI(SoundEvents.NOTE_BLOCK_PLING, 1f))
-                timer = -1
-                //#if CHEAT
-                ragnarockUsed = false
-                //#endif
+                timer = - 1
             }
         }
 
         register<WorldChangeEvent> {
-            currentLivid = null
-            timer = -1
-            //#if CHEAT
-            ragnarockUsed = false
-            //#endif
+            lividId = null
+            timer = - 1
         }
     }
+
+    private val Int?.livid get() = this?.let { mc.level?.getEntity(it) as? AbstractClientPlayer }
 }
