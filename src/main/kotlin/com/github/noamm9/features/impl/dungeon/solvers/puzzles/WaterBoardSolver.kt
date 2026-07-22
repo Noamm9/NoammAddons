1783f2fd422279d0fdc75eccc9b65c7c13918354
Noamm9 <belkunsama@gmail.com>
2026-07-22 15:02:17 +0300

    fix(WaterBoardSolver) doesnt register shift clicks

--- src/main/kotlin/com/github/noamm9/features/impl/dungeon/solvers/puzzles/WaterBoardSolver.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/solvers/puzzles/WaterBoardSolver.kt
@@ -1,6 +1,5 @@
 package com.github.noamm9.features.impl.dungeon.solvers.puzzles
 
-import com.github.noamm9.NoammAddons.mc
 import com.github.noamm9.event.impl.DungeonEvent
 import com.github.noamm9.event.impl.PlayerInteractEvent
 import com.github.noamm9.features.impl.dungeon.solvers.PuzzleSolvers
@@ -102,7 +101,6 @@ object WaterBoardSolver: PuzzleSolver {
     override fun onInteract(event: PlayerInteractEvent.RIGHT_CLICK.BLOCK) {
         if (patternId == - 1) return
         if (solution.isEmpty()) return
-        if (mc.player?.isCrouching == true) return
         val center = center ?: return
         val rotation = rotation ?: return
         val block = WorldUtils.getBlockAt(event.pos)
