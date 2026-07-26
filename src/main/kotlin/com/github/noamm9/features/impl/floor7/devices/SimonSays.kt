a6a3a036cd87298ce9e14b040642c593a6deb1bb
Noamm9 <belkunsama@gmail.com>
2026-07-26 14:15:40 +0300

    fix double category & reorder setting

--- src/main/kotlin/com/github/noamm9/features/impl/floor7/devices/SimonSays.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/floor7/devices/SimonSays.kt
@@ -9,7 +9,6 @@ import com.github.noamm9.ui.clickgui.components.impl.ColorSetting
 import com.github.noamm9.ui.clickgui.components.impl.SliderSetting
 import com.github.noamm9.ui.clickgui.components.impl.ToggleSetting
 import com.github.noamm9.utils.ChatUtils
-import com.github.noamm9.utils.ColorUtils.withAlpha
 import com.github.noamm9.utils.MathUtils.add
 import com.github.noamm9.utils.MathUtils.toVec
 import com.github.noamm9.utils.PlayerUtils
@@ -22,9 +21,9 @@ import com.github.noamm9.utils.render.RenderContext
 import net.minecraft.core.BlockPos
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.ButtonBlock
 import java.awt.Color
 import java.util.*
-import net.minecraft.world.level.block.ButtonBlock
 
 object SimonSays: Feature("Simon Says Solver") {
     private val ssSkip by ToggleSetting("SS skip Compatibility", true).withDescription("Always assume at the start that you perfectly SS skip").section("Options")
@@ -36,11 +35,10 @@ object SimonSays: Feature("Simon Says Solver") {
     private val phase by ToggleSetting("Phase", true).withDescription("Renders the box through walls.")
 
     //#if CHEAT
-    private val autoStart by ToggleSetting("Auto Start", false).withDescription("Automatically starts the device when it can be started.").section("Auto")
+    private val triggerBot by ToggleSetting("Triggerbot", false).withDescription("Automatically clicks the correct button when you're aiming at it.").section("Auto")
+    private val autoStart by ToggleSetting("Auto Start", false).withDescription("Automatically starts the device when it can be started.")
     private val startClicks by SliderSetting("Start Clicks", 3, 1, 10, 1).withDescription("Amount of clicks to start the device.").showIf { autoStart.value }
     private val startClickDelay by SliderSetting("Start Click Delay", 3, 1, 25, 1).withDescription("Delay in ticks between each start click.").showIf { autoStart.value }
-
-    private val triggerBot by ToggleSetting("Triggerbot", false).withDescription("Automatically clicks the correct button when you're aiming at it.").section("Auto")
     //#endif
 
     private val alertsEnabled by ToggleSetting("Alerts Enabled", true).section("Alerts")
@@ -93,7 +91,7 @@ object SimonSays: Feature("Simon Says Solver") {
             if (PlayerUtils.getSelectionBlock() != expected.button) return@register
 
             PlayerUtils.rightClick()
-}
+        }
         //#endif
 
         register<BlockChangeEvent> {
