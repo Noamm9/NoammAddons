eb11c7ca3c19079e11a3703d453abf625c5224f2
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-08-04 17:56:54 +0200

    fix(EnchantColors): nvm 0.05 steps are better
    last one frfr

--- src/main/kotlin/com/github/noamm9/features/impl/general/EnchantColors.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/general/EnchantColors.kt
@@ -26,7 +26,7 @@ object EnchantColors: Feature("Changes the color of enchantments in items lore."
     private val boldMaxLevel by ToggleSetting("Bold Max Level", true).withDescription("Make max level bold")
     private val rainbowMaxLevel by ToggleSetting("Rainbow Max Level").withDescription("Animate max level enchants with a rainbow effect")
     private val rainbowSpeed by SliderSetting("Rainbow Speed", 1.5, 0.1, 3, 0.1).showIf { rainbowMaxLevel.value }
-    private val rainbowSaturation by SliderSetting("Rainbow Saturation", 1.0, 0.0, 1.0, 0.1).showIf { rainbowMaxLevel.value }
+    private val rainbowSaturation by SliderSetting("Rainbow Saturation", 1.0, 0.0, 1.0, 0.05).showIf { rainbowMaxLevel.value }
 
     private val maxLevelColor by ColorSetting("Max Level Color", Color(255, 170, 0), false).hideIf { rainbowMaxLevel.value }.section("Colors")
     private val highLevelColor by ColorSetting("High Level Color", Color(255, 170, 0), false)
