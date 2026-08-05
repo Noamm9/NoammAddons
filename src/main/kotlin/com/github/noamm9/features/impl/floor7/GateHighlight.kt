83580d95fcd259c77f22ec3a0820bf8ade8f8ca8
Noamm9 <belkunsama@gmail.com>
2026-08-05 20:45:02 +0300

    refactor: change default colors, use WorldUtils.getBlockAt & equalsOneOf

--- src/main/kotlin/com/github/noamm9/features/impl/floor7/GateHighlight.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/floor7/GateHighlight.kt
@@ -5,6 +5,8 @@ import com.github.noamm9.features.Feature
 import com.github.noamm9.ui.clickgui.components.impl.ColorSetting
 import com.github.noamm9.utils.ColorUtils.withAlpha
 import com.github.noamm9.utils.MathUtils.aabb
+import com.github.noamm9.utils.WorldUtils
+import com.github.noamm9.utils.equalsOneOf
 import com.github.noamm9.utils.location.LocationUtils
 import com.github.noamm9.utils.render.Render3D.renderBoxBounds
 import net.minecraft.core.BlockPos
@@ -13,26 +15,24 @@ import net.minecraft.world.phys.AABB
 import java.awt.Color
 
 object GateHighlight: Feature("Highlights Gate as long as its not destroyed") {
-    private data class Gate(val pos: BlockPos, val box: AABB)
+    private val outlineColor by ColorSetting("Outline", Color.RED.withAlpha(255))
+    private val fillColor by ColorSetting("Fill", Color.RED.withAlpha(60))
 
     private val gates = mapOf(
         1 to Gate(BlockPos(103, 134, 123), aabb(95, 114, 122, 106, 134, 124)),
-        2 to Gate(BlockPos(17, 134, 135), aabb(18, 114, 126, 19, 134, 138)),
+        2 to Gate(BlockPos(17, 134, 135), aabb(18, 114, 127, 19, 134, 138)),
         3 to Gate(BlockPos(5, 134, 49), aabb(14, 114, 51, 1, 134, 49))
     )
 
-    private val outlineColor by ColorSetting("Outline", Color.CYAN.withAlpha(255))
-    private val fillColor by ColorSetting("Fill", Color.CYAN.withAlpha(60))
-
     override fun init() {
         register<RenderWorldEvent> {
-            if (! LocationUtils.inBoss || LocationUtils.F7Phase != 3) return@register
+            if (LocationUtils.F7Phase != 3) return@register
             val gate = gates[LocationUtils.P3Section] ?: return@register
-
-            val block = level.getBlockState(gate.pos).block
-            if (block != Blocks.CRACKED_STONE_BRICKS && block != Blocks.INFESTED_STONE_BRICKS) return@register
+            if (! WorldUtils.getBlockAt(gate.pos).equalsOneOf(Blocks.CRACKED_STONE_BRICKS, Blocks.INFESTED_STONE_BRICKS)) return@register
 
             event.ctx.renderBoxBounds(gate.box, outlineColor.value, fillColor.value)
         }
     }
+
+    private class Gate(val pos: BlockPos, val box: AABB)
 }
