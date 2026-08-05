bb4f5149f4e0a3cd0af9d433ea0b8a40ae9600bd
yavinyu <generalspecki@gmail.com>
2026-08-05 18:16:57 +0200

    feat: make gatehighlight customazible

--- src/main/kotlin/com/github/noamm9/features/impl/floor7/GateHighlight.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/floor7/GateHighlight.kt
@@ -3,6 +3,7 @@ package com.github.noamm9.features.impl.floor7
 import com.github.noamm9.NoammAddons
 import com.github.noamm9.event.impl.RenderWorldEvent
 import com.github.noamm9.features.Feature
+import com.github.noamm9.ui.clickgui.components.impl.ColorSetting
 import com.github.noamm9.utils.ColorUtils.withAlpha
 import com.github.noamm9.utils.MathUtils.aabb
 import com.github.noamm9.utils.location.LocationUtils
@@ -15,6 +16,9 @@ object GateHighlight: Feature("Highlights Gate as long as its not destroyed") {
     private val triggerPos = BlockPos(103, 134, 123)
     private val box = aabb(95, 114, 122, 106, 134, 124)
 
+    private val outlineColor by ColorSetting("Outline", Color.CYAN.withAlpha(255))
+    private val fillColor by ColorSetting("Fill", Color.CYAN.withAlpha(60))
+
     private val debugBypass get() = NoammAddons.debugFlags.contains("gate")
 
     override fun init() {
@@ -24,7 +28,7 @@ object GateHighlight: Feature("Highlights Gate as long as its not destroyed") {
             val block = level.getBlockState(triggerPos).block
             if (block != Blocks.CRACKED_STONE_BRICKS && block != Blocks.INFESTED_STONE_BRICKS) return@register
 
-            event.ctx.renderBoxBounds(box, Color.CYAN.withAlpha(255), Color.CYAN.withAlpha(60))
+            event.ctx.renderBoxBounds(box, outlineColor.value, fillColor.value)
         }
     }
 }
