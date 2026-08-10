59f719a176c647fcfdcb2089987708e2db83f586
arilee <arilee7x@gmail.com>
2026-08-11 02:40:09 +0300

    pet menu bug fix
    when selecting a pet in a vanilla menu custom wasnt reverting back on next open

--- src/main/kotlin/com/github/noamm9/features/impl/visual/PetMenu.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/visual/PetMenu.kt
@@ -128,6 +128,11 @@ object PetMenu: Feature("Replaces the Pets inventory with a custom pet wheel."),
             wheelPage = 0
             tempDisabled = false
         }
+
+        register<ContainerEvent.Open> {
+            if (! event.screen.title.unformattedText.matches(petMenuRegex)) return@register
+            tempDisabled = false
+        }
     }
 
     private fun renderWheel(ctx: GuiGraphicsExtractor, screen: AbstractContainerScreen<*>, vanillaMouseX: Int, vanillaMouseY: Int) {
