8c7959640b092c05d6db5b2257dfea75d21dd4d8
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-08-15 15:07:15 +0200

    chore: merge AbilityKeybinds & AutoUlt into Abilities module

---
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/Abilities.kt
@@ -0,0 +1,90 @@
+package com.github.noamm9.features.impl.dungeon
+
+import com.github.noamm9.event.impl.KeyboardEvent
+import com.github.noamm9.features.Feature
+import com.github.noamm9.ui.clickgui.components.impl.KeybindSetting
+import com.github.noamm9.ui.clickgui.components.impl.ToggleSetting
+import com.github.noamm9.utils.PlayerUtils
+import com.github.noamm9.utils.dungeons.DungeonListener
+import com.github.noamm9.utils.location.LocationUtils
+import org.lwjgl.glfw.GLFW
+
+//#if CHEAT
+import com.github.noamm9.event.impl.ChatMessageEvent
+import com.github.noamm9.utils.ChatUtils
+import com.github.noamm9.utils.dungeons.enums.DungeonClass
+//#endif
+
+object Abilities: Feature(
+    //#if CHEAT
+    "Allows you to use dungeon class abilities with keybinds and automatically trigger ultimates when needed."
+    //#else
+    //$"Allows you to use dungeon class abilities with keybinds."
+    //#endif
+) {
+    private val classUltimate by ToggleSetting("Class Ultimate").section("Keybinds")
+    private val ultKeybind by KeybindSetting("Ultimate Keybind").showIf { classUltimate.value }
+    private val classAbility by ToggleSetting("Class Ability")
+    private val abilityKeybind by KeybindSetting("Ability Keybind").showIf { classAbility.value }
+
+    //#if CHEAT
+    private val autoUlt by ToggleSetting("Auto Use Ultimate").section("Auto Ultimate")
+
+    private class UltMessage(val msg: String, val classes: List<DungeonClass>, val floor: Int)
+
+    private val ultMessages = listOf(
+        UltMessage(
+            msg = "⚠ Maxor is enraged! ⚠",
+            classes = listOf(DungeonClass.Healer, DungeonClass.Tank),
+            floor = 7
+        ),
+        UltMessage(
+            msg = "[BOSS] Goldor: You have done it, you destroyed the factory…",
+            classes = listOf(DungeonClass.Healer, DungeonClass.Tank),
+            floor = 7
+        ),
+        UltMessage(
+            msg = "[BOSS] Sadan: My giants! Unleashed!",
+            classes = listOf(DungeonClass.Healer, DungeonClass.Tank, DungeonClass.Archer, DungeonClass.Berserk, DungeonClass.Mage),
+            floor = 6
+        ),
+        UltMessage(
+            msg = "[BOSS] Livid: I respect you for making it to here, but I'll be your undoing.",
+            classes = listOf(DungeonClass.Healer, DungeonClass.Tank),
+            floor = 5
+        )
+    )
+    //#endif
+
+    override fun init() {
+        register<KeyboardEvent.KeyPressed> {
+            if (! LocationUtils.inDungeon || ! DungeonListener.dungeonStarted) return@register
+            if (event.action != GLFW.GLFW_PRESS) return@register
+            if (mc.screen != null) return@register
+
+            if (classUltimate.value && ultKeybind.isPressed()) {
+                event.isCanceled = true
+                return@register PlayerUtils.useDungeonClassAbility(true)
+            }
+
+            if (classAbility.value && abilityKeybind.isPressed()) {
+                event.isCanceled = true
+                return@register PlayerUtils.useDungeonClassAbility(false)
+            }
+        }
+
+        //#if CHEAT
+        register<ChatMessageEvent> {
+            if (! autoUlt.value || ! LocationUtils.inBoss) return@register
+            val msg = event.unformattedText
+            val matchingMessage = ultMessages.find {
+                it.msg == msg && it.floor == LocationUtils.dungeonFloorNumber
+            } ?: return@register
+
+            if (DungeonListener.thePlayer?.clazz !in matchingMessage.classes) return@register
+            PlayerUtils.useDungeonClassAbility(true)
+            ChatUtils.modMessage("Used Ultimate!")
+        }
+        //#endif
+    }
+}
