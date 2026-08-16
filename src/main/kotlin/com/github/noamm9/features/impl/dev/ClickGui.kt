7a5aebd70bfa5f91e9eb6cd48917838afddd5fd9
Noamm9 <belkunsama@gmail.com>
2026-08-16 03:37:54 +0300

    cleanup

--- src/main/kotlin/com/github/noamm9/features/impl/dev/ClickGui.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/dev/ClickGui.kt
@@ -4,25 +4,16 @@ import com.github.noamm9.event.impl.KeyboardEvent
 import com.github.noamm9.event.impl.MouseClickEvent
 import com.github.noamm9.features.Feature
 import com.github.noamm9.ui.clickgui.ClickGuiScreen
-import com.github.noamm9.ui.clickgui.components.impl.ButtonSetting
-import com.github.noamm9.ui.clickgui.components.impl.ColorSetting
-import com.github.noamm9.ui.clickgui.components.impl.DropdownSetting
-import com.github.noamm9.ui.clickgui.components.impl.KeybindSetting
-import com.github.noamm9.ui.clickgui.components.impl.ToggleSetting
+import com.github.noamm9.ui.clickgui.components.impl.*
 import com.github.noamm9.ui.hud.HudEditorScreen
 import com.github.noamm9.utils.GuiUtils
 import org.lwjgl.glfw.GLFW
 import java.awt.Color
 
 object ClickGui: Feature("A feature used to change the ClickGui configuration.", toggled = true) {
-    val playClickSound by ToggleSetting("Click Sound", true)
-        .withDescription("Toggle for the sound that plays when you click on a setting element.")
-
-    val accentColor by ColorSetting("Accent Color", Color(99, 176, 217), false)
-        .withDescription("The accent color used by the whole ClickGui.")
-
-    val panelSorting by DropdownSetting("Sorting", 1, listOf("A-Z Sorting", "Width Sorting", "No Sorting"))
-        .withDescription("The order of the features in the panels.")
+    val playClickSound by ToggleSetting("Click Sound", true).withDescription("Toggle for the sound that plays when you click on a setting element.")
+    val accentColor by ColorSetting("Accent Color", Color(99, 176, 217), false).withDescription("The accent color used by the whole ClickGui.")
+    val panelSorting by DropdownSetting("Sorting", 1, listOf("A-Z Sorting", "Width Sorting", "No Sorting")).withDescription("The order of the features in the panels.")
 
     val editGuiButton by ButtonSetting("Open HUD Editor") {
         ClickGuiScreen.INSTANCE?.onClose()
@@ -54,5 +45,5 @@ object ClickGui: Feature("A feature used to change the ClickGui configuration.",
         }
     }
 
-    override fun toggle() {}
+    override fun toggle() = Unit
 }
