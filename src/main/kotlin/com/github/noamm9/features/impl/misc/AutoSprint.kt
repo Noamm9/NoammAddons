50ff23af842d8722ed85128c9b58480c42499c4e
Noamm9 <belkunsama@gmail.com>
2026-08-21 18:49:29 +0300

    feat(AutoSprint) option to temp disable while in water

--- src/main/kotlin/com/github/noamm9/features/impl/misc/AutoSprint.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/misc/AutoSprint.kt
@@ -1,13 +1,17 @@
 package com.github.noamm9.features.impl.misc
 
+import com.github.noamm9.config.types.ToggleSetting
 import com.github.noamm9.event.impl.TickEvent
 import com.github.noamm9.features.Feature
 
 object AutoSprint: Feature("Automatically sprint for you.") {
+    private val disableInWater by ToggleSetting("Disable In Water").withDescription("Stops sprinting while you are in water.")
+
     override fun init() {
         register<TickEvent.Start> {
             if (mc.screen != null) return@register
             if (player.isSprinting) return@register
+            if (disableInWater.value && player.isInWater) return@register
             mc.options.keySprint.isDown = true
         }
     }
