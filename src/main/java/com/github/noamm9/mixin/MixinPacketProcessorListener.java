c909d613daadca03db1b852dca5e658c9729e0eb
Noamm9 <belkunsama@gmail.com>
2026-08-24 14:33:30 +0300

    cleanup MixinPacketProcessorListener

--- src/main/java/com/github/noamm9/mixin/MixinPacketProcessorListener.java
+++ src/main/java/com/github/noamm9/mixin/MixinPacketProcessorListener.java
@@ -2,41 +2,23 @@ package com.github.noamm9.mixin;
 
 import com.github.noamm9.event.EventBus;
 import com.github.noamm9.event.impl.MainThreadPacketReceivedEvent;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.network.PacketListener;
 import net.minecraft.network.protocol.Packet;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net.minecraft.network.PacketProcessor$ListenerAndPacket")
-public class MixinPacketProcessorListener {
-    @Shadow @Final private Packet<?> packet;
+public class MixinPacketProcessorListener<T extends PacketListener> {
+    @Shadow @Final private Packet<T> packet;
 
-    @Inject(
-        method = "handle",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/network/protocol/Packet;handle(Lnet/minecraft/network/PacketListener;)V"
-        ),
-        cancellable = true
-    )
-    private void onPreHandle(CallbackInfo ci) {
-        if (EventBus.post(new MainThreadPacketReceivedEvent.Pre(this.packet))) {
-            ci.cancel();
-        }
-    }
-
-    @Inject(
-        method = "handle",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/network/protocol/Packet;handle(Lnet/minecraft/network/PacketListener;)V",
-            shift = At.Shift.AFTER
-        )
-    )
-    private void onPostHandle(CallbackInfo ci) {
-        EventBus.post(new MainThreadPacketReceivedEvent.Post(this.packet));
+    @WrapOperation(method = "handle", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/Packet;handle(Lnet/minecraft/network/PacketListener;)V"))
+    private void onHandle(Packet<T> instance, T listener, Operation<Void> original) {
+        if (EventBus.post(new MainThreadPacketReceivedEvent.Pre(packet))) return;
+        original.call(instance, listener);
+        EventBus.post(new MainThreadPacketReceivedEvent.Post(packet));
     }
 }
