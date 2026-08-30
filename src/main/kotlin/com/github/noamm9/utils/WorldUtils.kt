636a4be2da02ffeb5678e514e4f6d89d548f5b3e
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-08-30 15:13:15 +0200

    chore: add bobby compability
    also typo fix

--- src/main/kotlin/com/github/noamm9/utils/WorldUtils.kt
+++ src/main/kotlin/com/github/noamm9/utils/WorldUtils.kt
@@ -8,7 +8,11 @@ import net.minecraft.world.level.chunk.status.ChunkStatus
 import net.minecraft.world.phys.Vec3
 
 object WorldUtils {
-    fun getStateAt(pos: BlockPos) = NoammAddons.mc.level?.getBlockState(pos) ?: Blocks.AIR.defaultBlockState()
+    private fun getLoadedChunk(chunkX: Int, chunkZ: Int) = NoammAddons.mc.level
+        ?.getChunk(chunkX, chunkZ, ChunkStatus.FULL, false)
+        ?.takeUnless { it.javaClass.name == "de.johni0702.minecraft.bobby.FakeChunk" }
+
+    fun getStateAt(pos: BlockPos) = getLoadedChunk(pos.x shr 4, pos.z shr 4)?.getBlockState(pos) ?: Blocks.AIR.defaultBlockState()
     fun getStateAt(x: Int, y: Int, z: Int) = this.getStateAt(BlockPos(x, y, z))
     fun getBlockAt(pos: BlockPos) = getStateAt(pos).block
     fun getBlockAt(vec3: Vec3) = getBlockAt(BlockPos(vec3.x.toInt(), vec3.y.toInt(), vec3.z.toInt()))
@@ -17,12 +21,11 @@ object WorldUtils {
     fun setBlockAt(pos: BlockPos, state: BlockState) = NoammAddons.mc.level?.setBlock(pos, state, 19)
 
     fun isChunkLoaded(x: Number, z: Number): Boolean {
-        return NoammAddons.mc.level?.getChunk(x.toInt() shr 4, z.toInt() shr 4, ChunkStatus.FULL, false) != null
+        return getLoadedChunk(x.toInt() shr 4, z.toInt() shr 4) != null
     }
 
     fun getBlockEntityList(): List<BlockPos> {
         val player = NoammAddons.mc.player ?: return emptyList()
-        val level = NoammAddons.mc.level ?: return emptyList()
         val renderDistance = NoammAddons.mc.options.renderDistance().get()
         val pX = player.chunkPosition().x
         val pZ = player.chunkPosition().z
@@ -30,7 +33,7 @@ object WorldUtils {
         return buildList {
             for (x in (pX - renderDistance) .. (pX + renderDistance)) {
                 for (z in (pZ - renderDistance) .. (pZ + renderDistance)) {
-                    val chunk = level.getChunk(x, z, ChunkStatus.FULL, false) ?: continue
+                    val chunk = getLoadedChunk(x, z) ?: continue
                     addAll(chunk.blockEntitiesPos)
                 }
             }
