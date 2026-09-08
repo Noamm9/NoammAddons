288ffa830899e95db945712124715ee4c6919129
Noamm9 <belkunsama@gmail.com>
2026-09-08 23:28:58 +0300

    refactor

--- src/main/kotlin/com/github/noamm9/features/impl/dungeon/RoomAlerts.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/RoomAlerts.kt
@@ -1,5 +1,6 @@
 package com.github.noamm9.features.impl.dungeon
 
+import com.github.noamm9.config.types.SliderSetting
 import com.github.noamm9.config.types.ToggleSetting
 import com.github.noamm9.event.impl.DungeonEvent
 import com.github.noamm9.event.impl.WorldChangeEvent
@@ -13,38 +14,31 @@ import com.github.noamm9.utils.render.Render2D.drawCenteredString
 import com.github.noamm9.utils.render.RenderHelper.width
 import gg.essential.universal.USound
 import net.minecraft.sounds.SoundEvents
+import kotlin.math.roundToLong
 
 object RoomAlerts: Feature("Alerts when certain stuff happens in your current room") {
     private val clear by ToggleSetting("Cleared", true)
     private val secrets by ToggleSetting("Secrets Done", true)
+    private val displayTime by SliderSetting("Display Time", 2.0, 0.5, 3.0, 0.1, "s")
 
-    private var clearText = "Cleared"
-    private var clearUntil = 0L
-    private var secretsUntil = 0L
+    private var alertText = "Cleared"
+        set(value) {
+            field = value
+            USound.playSoundStatic(SoundEvents.NOTE_BLOCK_PLING, 0.25f, 1f)
+            alertUntil = System.currentTimeMillis() + (displayTime.value * 1000).roundToLong()
+        }
+
+    private var alertUntil = 0L
 
     override fun init() {
-        val clearHud = hudElement("Room Alerts Cleared", { clear.value }, { System.currentTimeMillis() < clearUntil }, centered = true) { ctx, example ->
-            val text = if (example) "Cleared" else clearText
+        hudElement("Room Alerts", { clear.value || secrets.value }, { System.currentTimeMillis() < alertUntil }, centered = true) { ctx, example ->
+            val text = if (example) "Cleared" else alertText
             ctx.drawCenteredString(text, 0, 0)
             text.width() to 9f
-        }
-        val secretsHud = hudElement("Room Alerts Secrets Done", { secrets.value }, { System.currentTimeMillis() < secretsUntil }, centered = true) { ctx, _ ->
-            val text = "&aSecrets Done!"
-            ctx.drawCenteredString(text, 0, 0)
-            text.width() to 9f
-        }
-        listOf(clearHud, secretsHud).forEachIndexed { index, hud ->
-            hud.defaults {
-                x = Resolution.width / 2f
-                y = Resolution.height * 0.444f + index * 30f
-                scale = 2.5f
-            }
-            hud.defaults.invoke(hud)
-        }
-
-        register<WorldChangeEvent> {
-            clearUntil = 0L
-            secretsUntil = 0L
+        } defaults {
+            x = Resolution.width / 2f
+            y = Resolution.height * 0.444f
+            scale = 2.5f
         }
 
         register<DungeonEvent.RoomEvent.onStateChange> {
@@ -53,21 +47,13 @@ object RoomAlerts: Feature("Alerts when certain stuff happens in your current ro
             if (event.room.data.type == RoomType.PUZZLE && event.room.name != "Blaze") return@register
             if (DungeonListener.thePlayer !in event.roomPlayers) return@register
 
-            when (event.newState) {
-                RoomState.CLEARED -> {
-                    if (! clear.value) return@register
-                    clearText = (if (event.room.data.secrets == 0) "&a" else "") + "Cleared"
-                    clearUntil = System.currentTimeMillis() + 2000
-                }
-
-                RoomState.GREEN -> {
-                    if (! secrets.value || event.room.data.secrets <= 0) return@register
-                    secretsUntil = System.currentTimeMillis() + 2000
-                }
-
+            alertText = when (event.newState) {
+                RoomState.CLEARED if clear.value -> (if (event.room.data.secrets == 0) "&a" else "") + "Cleared"
+                RoomState.GREEN if (secrets.value && event.room.data.secrets > 0) -> "&aSecrets Done!"
                 else -> return@register
             }
-            USound.playSoundStatic(SoundEvents.NOTE_BLOCK_PLING, 0.25f, 1f)
         }
+
+        register<WorldChangeEvent> { alertUntil = 0L }
     }
 }
