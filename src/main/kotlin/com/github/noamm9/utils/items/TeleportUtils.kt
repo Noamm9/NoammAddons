4dc6b3ce76ca572d967730722b87cbde528eb36c
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-09-09 17:42:55 +0200

    feat(NoRotate): add etherwarp conduit support

--- src/main/kotlin/com/github/noamm9/utils/items/TeleportUtils.kt
+++ src/main/kotlin/com/github/noamm9/utils/items/TeleportUtils.kt
@@ -45,6 +45,7 @@ object TeleportUtils: Shortcuts {
     fun getInfo(stack: ItemStack?): Info? {
         if (stack == null || stack.isEmpty) return null
         val sbId = stack.skyblockId
+        if (sbId == "ETHERWARP_CONDUIT") return EtherwarpHelper.getEtherwarpDistance(stack)?.let { Info(it, Etherwarp) }
         val nbt = stack.customData
 
         if (sbId.equalsOneOf("ASPECT_OF_THE_VOID", "ASPECT_OF_THE_END")) {
