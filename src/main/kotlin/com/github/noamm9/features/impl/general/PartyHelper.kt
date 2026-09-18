2dc3bc0efd0205b7ee832a020064867b03aec8e5
admin <dsgameover@users.noreply.github.com>
2026-09-18 18:23:22 +0300

    zzz

--- src/main/kotlin/com/github/noamm9/features/impl/general/PartyHelper.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/general/PartyHelper.kt
@@ -128,9 +128,9 @@ object PartyHelper: Feature("Party commands and reformatting."), ICommandProvide
                 if (floor in 0 .. 7) runCommand("joininstance CATACOMBS_FLOOR_${DungeonUtils.FLOOR_NAMES[floor]}", true)
             }
 
-            commands["!f"] && cmd.matches(Regex("^m\\d*$")) -> {
+            commands["!m"] && cmd.matches(Regex("^m\\d*$")) -> {
                 val floor = cmd.removePrefix("m").toIntOrNull() ?: args.getOrNull(0)?.toIntOrNull() ?: return
-                if (floor in 0 .. 7) runCommand("joininstance MASTER_CATACOMBS_FLOOR_${DungeonUtils.FLOOR_NAMES[floor]}", true)
+                if (floor in 1 .. 7) runCommand("joininstance MASTER_CATACOMBS_FLOOR_${DungeonUtils.FLOOR_NAMES[floor]}", true)
             }
 
             commands["!pt"] && cmd.equalsOneOf("pt", "ptme") -> {
