cded6882214c3368b36aecd72ea189140a24625a
Timo <tjiba@tjiba.fr>
2026-09-24 00:10:03 +0200

    Bp/ec Custom Names

--- src/main/kotlin/com/github/noamm9/features/impl/general/storageoverlay/StoragePage.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/general/storageoverlay/StoragePage.kt
@@ -5,7 +5,10 @@ import com.github.noamm9.utils.ChatUtils
 data class StoragePage(val index: Int): Comparable<StoragePage> {
     val isEnderChest = index < 9
     val isBackPack = ! isEnderChest
-    val name = if (isEnderChest) "Ender Chest #${index + 1}" else "Backpack #${index - 9 + 1}"
+    val defaultName = if (isEnderChest) "Ender Chest #${index + 1}" else "Backpack #${index - 9 + 1}"
+    val customName get() = StorageOverlay.storageNames.value[index]?.name?.takeUnless { it.isBlank() || it.trim() == defaultName }
+    val name get() = if (StorageOverlay.shouldShowCustomName(this)) customName ?: defaultName else defaultName
+    val color get() = if (StorageOverlay.shouldShowCustomName(this)) StorageOverlay.storageNames.value[index]?.color else null
 
     fun open() = ChatUtils.sendCommand(if (isBackPack) "backpack ${index - 9 + 1}" else "enderchest ${index + 1}")
     override fun compareTo(other: StoragePage) = this.index - other.index
