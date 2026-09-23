c0b1f125208362ed516271e3dc1fccc19694a91f
AxeOfTheShredded <alfaslayer69@gmail.com>
2026-09-22 19:33:20 -0700

    no boss bar

--- src/main/java/com/github/noamm9/mixin/MixinBossHealthOverlay.java
+++ src/main/java/com/github/noamm9/mixin/MixinBossHealthOverlay.java
@@ -1,18 +1,18 @@
 package com.github.noamm9.mixin;
 
-import com.github.noamm9.features.impl.dungeon.BossBarHealth;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.gui.components.BossHealthOverlay;
-import net.minecraft.client.gui.components.LerpingBossEvent;
-import net.minecraft.network.chat.Component;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
+import net.minecraft.client.gui.components.BossHealthOverlay;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import com.github.noamm9.features.impl.visual.RenderOptimizer;
 @Mixin(BossHealthOverlay.class)
 public class MixinBossHealthOverlay {
-    @WrapOperation(method = "extractRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/LerpingBossEvent;getName()Lnet/minecraft/network/chat/Component;"))
-    public Component onRender(LerpingBossEvent instance, Operation<Component> original) {
-        return BossBarHealth.onRender(instance, original);
+    @ModifyConstant(method = "*", constant = @Constant(intValue = 182))
+    private int injected(int value) {
+        if(RenderOptimizer.INSTANCE.getHideBossBar().getValue() && RenderOptimizer.INSTANCE.enabled){
+            return 0;
+        }
+        return value;
     }
 }
