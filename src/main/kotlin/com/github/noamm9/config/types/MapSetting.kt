d0014b9e6e247b1e4d6f648d0bdc72094eee706c
Timo <tjiba@tjiba.fr>
2026-09-25 20:51:19 +0200

    fix(StorageOverlay): preserve custom names after restart
    Pass explicit map types to Gson so page indices reload as integers

--- src/main/kotlin/com/github/noamm9/config/types/MapSetting.kt
+++ src/main/kotlin/com/github/noamm9/config/types/MapSetting.kt
@@ -3,13 +3,11 @@ package com.github.noamm9.config.types
 import com.github.noamm9.config.ConfigHolder
 import com.github.noamm9.config.Savable
 import com.github.noamm9.utils.GsonUtils
-import com.google.common.reflect.TypeToken
 import com.google.gson.JsonElement
+import java.lang.reflect.Type
 
-class MapSetting<K, V>(name: String, defaults: MutableMap<K, V> = mutableMapOf()): ConfigHolder<MutableMap<K, V>>(name, defaults.toMutableMap()), Savable {
-    constructor(name: String, vararg defaults: Pair<K, V>): this(name, defaults.toMap().toMutableMap())
-
-    private val type = object: TypeToken<MutableMap<K, V>>() {}.type
+class MapSetting<K, V>(name: String, defaults: MutableMap<K, V> = mutableMapOf(), private val type: Type): ConfigHolder<MutableMap<K, V>>(name, defaults.toMutableMap()), Savable {
+    constructor(name: String, vararg defaults: Pair<K, V>, type: Type): this(name, defaults.toMap().toMutableMap(), type)
 
     override fun write() = GsonUtils.gson.toJsonTree(value, type)
     override fun read(element: JsonElement) = ::value.set(GsonUtils.gson.fromJson(element, type))
--- src/test/kotlin/config/MapSettingTest.kt
+++ src/test/kotlin/config/MapSettingTest.kt
@@ -1,13 +1,14 @@
 package config
 
 import com.github.noamm9.config.types.MapSetting
+import com.google.common.reflect.TypeToken
 import com.google.gson.JsonParser
 import kotlin.test.*
 
 class MapSettingTest {
     @Test
     fun `writes integer keys and string values`() {
-        val setting = MapSetting("Names", mutableMapOf(0 to "&aFarming", 26 to "Tools"))
+        val setting = MapSetting("Names", mutableMapOf(0 to "&aFarming", 26 to "Tools"), object: TypeToken<MutableMap<Int, String>>() {}.type)
         val json = setting.write().asJsonObject
         assertEquals("&aFarming", json.get("0").asString)
         assertEquals("Tools", json.get("26").asString)
@@ -15,14 +16,14 @@ class MapSettingTest {
 
     @Test
     fun `supports nested values`() {
-        val setting = MapSetting("Groups", mutableMapOf("slots" to listOf(1, 3, 5)))
+        val setting = MapSetting("Groups", mutableMapOf("slots" to listOf(1, 3, 5)), object: TypeToken<MutableMap<String, List<Int>>>() {}.type)
         assertEquals(JsonParser.parseString("""{"slots":[1,3,5]}"""), setting.write())
     }
 
     @Test
     fun `edits entries without changing the supplied defaults`() {
         val defaults = mutableMapOf(0 to "&aFarming")
-        val setting = MapSetting("Names", defaults)
+        val setting = MapSetting("Names", defaults, object: TypeToken<MutableMap<Int, String>>() {}.type)
         assertNull(setting[1])
         setting[0] = "&6Mining"
         setting[1] = "Tools"
@@ -36,7 +37,7 @@ class MapSettingTest {
 
     @Test
     fun `supports vararg constructor`() {
-        val setting = MapSetting("Names", 0 to "First", 1 to "Second")
+        val setting = MapSetting("Names", 0 to "First", 1 to "Second", type = object: TypeToken<MutableMap<Int, String>>() {}.type)
         assertEquals("First", setting[0])
         assertEquals("Second", setting[1])
         assertEquals(2, setting.value.size)
@@ -44,7 +45,7 @@ class MapSettingTest {
 
     @Test
     fun `triggers changeListener on set`() {
-        val setting = MapSetting("Names", 0 to "First")
+        val setting = MapSetting("Names", 0 to "First", type = object: TypeToken<MutableMap<Int, String>>() {}.type)
         var called = false
         setting.changeListener = { called = true }
         setting[0] = "Second"
